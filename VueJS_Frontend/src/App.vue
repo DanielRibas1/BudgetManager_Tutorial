4c579f278747bb4c68edc5fe8ac29645a9a2de4c
DanielRibas1 <daniribas01@gmail.com>
2018-05-10 19:54:54 +0200

    Authentication component

--- VueJS_Frontend/src/App.vue
+++ VueJS_Frontend/src/App.vue
@@ -3,13 +3,13 @@
     <v-container>
       <router-view/>
     </v-container>
-  </div>  
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'app'
-  }
+export default {
+  name: 'app'
+}
 </script>
 
 <style lang="scss">
