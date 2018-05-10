d2f7abcae6dc72df2a04fedeb7b263932e087cee
DanielRibas1 <daniribas01@gmail.com>
2018-05-10 12:45:13 +0200

    Added Consign config for API

--- FocusBudgetManager/services/BudgetManagerAPI/config/app.js
+++ FocusBudgetManager/services/BudgetManagerAPI/config/app.js
@@ -20,4 +20,10 @@ app.use(passport.initialize());
 
 app.set('budgetsecret', config.secret);
 
+consign({ cwd: 'services' })
+    .include('BudgetManagerAPI/app/setup')
+    .then('BudgetManagerAPI/app/api')
+    .then('BudgetManagerAPI/app/routes')
+    .into(app);
+
 module.exports = app;
