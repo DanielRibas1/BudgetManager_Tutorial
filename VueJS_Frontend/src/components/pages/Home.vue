5a5302af36ae02ff7243fbd1f7d5dce6c9413619
DanielRibas1 <daniribas01@gmail.com>
2018-05-10 20:20:02 +0200

    Adding navigation guard

--- VueJS_Frontend/src/components/pages/Home.vue
+++ VueJS_Frontend/src/components/pages/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <h3>Hi! this is our App's Home</h3>
     <ul>
-      <li v-if="users != null" v-for="user in users">
+      <li v-if="users != null" v-for="user in users" :key="user.username">
         {{ user.username }}
       </li>
     </ul>
@@ -10,23 +10,23 @@
 </template>
 
 <script>
-  import Axios from 'axios'
-  import Authentication from '@/components/pages/Authentication'
-  const BudgetManagerAPI = `http://${window.location.hostname}:3001`
-  export default {
-    data () {
-      return {
-        users: []
-      }
-    },
-    mounted: {
-      getAllUsers(context) {
-        Axios.get(`${BudgetManagerAPI}/api/v1/users`, {
-          headers: {
-            'Authorization' : Authentication.getAuthenticationHeader(this)
-          }
-        }).then(({data}) => (this.users = data))
-      }
+import Axios from 'axios'
+import Authentication from '@/components/pages/Authentication'
+const BudgetManagerAPI = `http://${window.location.hostname}:3001`
+export default {
+  data () {
+    return {
+      users: []
+    }
+  },
+  mounted: {
+    getAllUsers (context) {
+      Axios.get(`${BudgetManagerAPI}/api/v1/users`, {
+        headers: {
+          'Authorization': Authentication.getAuthenticationHeader(this)
+        }
+      }).then(({data}) => (this.users = data))
     }
   }
+}
 </script>
