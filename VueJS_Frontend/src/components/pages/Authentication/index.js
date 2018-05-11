cb45d06cb4870900ad81ff4bf69e608e1ef7cd6a
DanielRibas1 <daniribas01@gmail.com>
2018-05-11 11:24:41 +0200

    Some improving, adding signout and checkAuthenticate methods

--- VueJS_Frontend/src/components/pages/Authentication/index.js
+++ VueJS_Frontend/src/components/pages/Authentication/index.js
@@ -8,9 +8,11 @@ export default {
 
   authenticate (context, credentials, redirect) {
     Axios.post(`${BudgetManagerAPI}/api/v1/auth`, credentials)
-      .then(({ data: { token } }) => {
+      .then(({ data }) => {
         context.$cookie.set('token', token, '1D')
+        context.$cookie.set('user_id', data.user, '1D')
         context.validLogin = true
+
         this.user.authenticate = true
 
         if (redirect) router.push(redirect)
@@ -22,16 +24,27 @@ export default {
 
   signup (context, credentials, redirect) {
     Axios.post(`${BudgetManagerAPI}/api/v1/signup`, credentials)
-      .then(({ data: { token } }) => {
-        context.$cookie.set('token', token, '1D')
-        context.validLogin = true
-        this.user.authenticate = true
-
-        if (redirect) router.push(redirect)
+      .then(() => {
+        context.validSignUp = true       
+        
+        this.authenticate(context, credentials, redirect)        
       }).catch(({ response: { data } }) => {
         context.snackbar = true
         context.message = data.message
       })
+  },
+
+  signout (context, redirect) {
+    context.$cookie.delete('token')
+    context.$cookie.delete('user_id')
+    this.user.authenticated = false
+
+    if (redirect) router.push(redirect)
+  },
+
+  checkAuthentication () {
+    const token = document.cookie
+    this.user.authenticated = !!token
   },
 
   checkAuthentication () {
