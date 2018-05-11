9551af77d7a2caba8a18ed878927f4fc2b7ca9be
DanielRibas1 <daniribas01@gmail.com>
2018-05-11 12:50:51 +0200

    Adding new Models

---
+++ NodeJS_Backend/services/BudgetManagerAPI/app/models/client.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+mongoose.model('Client', Schema);
